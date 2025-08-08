704eb7743990aeeb4746562e70c5be5f9081736d
Zixuan Chen <remch183@outlook.com>
2025-08-08 14:12:15 +0800

    docs: refine the landing page

--- components/landing/Features.tsx
+++ components/landing/Features.tsx
@@ -5,6 +5,9 @@ export default function Features() {
   return (
     <section className="w-full px-5 md:px-15 pt-4 pb-20 md:pt-15 md:pb-15 relative z-10">
       <FollowOnGitHub className="visible md:hidden mb-10 mt-8" />
+      <p className="text-center text-neutral-400 mb-8">
+        Loro is a high‑performance CRDT library for local‑first, real‑time collaboration.
+      </p>
       <main className="flex flex-col space-y-5 gap-0 md:space-y-0 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-5 xl:grid-cols-4 xl:grid-rows-1">
         <article className={classes.Card}>
           <h3 className="text-base md:text-xl font-extrabold bg-blue-green bg-clip-text text-fill text-fill-transparent md:w-auto">
