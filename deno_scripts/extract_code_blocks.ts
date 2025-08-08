d9735def9562e86a7f3dad2d659ffd15e46a5a17
Zixuan Chen <remch183@outlook.com>
2025-08-08 16:25:55 +0800

    test: fix deno codeblocks tests (#22)

--- deno_scripts/extract_code_blocks.ts
+++ deno_scripts/extract_code_blocks.ts
@@ -11,7 +11,7 @@ export interface CodeBlock {
 
 export function extractCodeBlocks(fileContent: string, codeBlocks: CodeBlock[], name: string, path: string) {
   // Regular expression to detect TypeScript code blocks
-  const codeBlockRegex = /```(typescript|ts|js|javascript)\n([\s\S]*?)\n```/g;
+  const codeBlockRegex = /```(typescript|ts twoslash|typescript twoslash|ts|js|javascript)\n([\s\S]*?)\n```/g;
   let match;
   while ((match = codeBlockRegex.exec(fileContent)) !== null) {
     const startLine = fileContent.substring(0, match.index).split("\n").length;
