128fc20a3bff94502d8e041371865bea9b3739c8
Zixuan Chen <remch183@outlook.com>
2025-09-22 15:24:20 +0800

    blog: add intro to loro-mirror

--- pages/blog/_meta.js
+++ pages/blog/_meta.js
@@ -1,32 +1,38 @@
 export default {
+  "loro-mirror": {
+    theme: {
+      toc: true,
+      pagination: false,
+    },
+  },
   "v1.0": {
     theme: {
       toc: true,
-      pagination: false
-    }
+      pagination: false,
+    },
   },
   "movable-tree": {
     theme: {
       toc: true,
-      pagination: false
-    }
+      pagination: false,
+    },
   },
   "loro-richtext": {
     theme: {
       toc: true,
-      pagination: false
-    }
+      pagination: false,
+    },
   },
   "loro-now-open-source": {
     theme: {
       toc: true,
-      pagination: false
-    }
+      pagination: false,
+    },
   },
   "crdt-richtext": {
     theme: {
       toc: true,
-      pagination: false
-    }
-  }
-}
+      pagination: false,
+    },
+  },
+};
