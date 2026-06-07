85a90ede000faf54786f0a40bea8a99e0e1bfdda
Zixuan Chen <remch183@outlook.com>
2026-06-07 15:00:36 +0800

    fix: enable standalone Next output for OpenNext

--- next.config.mjs
+++ next.config.mjs
@@ -9,42 +9,43 @@ const withNextra = nextra({
 });
 
 export default withNextra({
-    webpack(config, { isServer, dev }) {
-      // Use the client static directory in the server bundle and prod mode
-      // Fixes `Error occurred prerendering page "/"`
-      config.output.webassemblyModuleFilename =
-        isServer && !dev
-          ? "../static/wasm/[modulehash].wasm"
-          : "static/wasm/[modulehash].wasm";
+  output: "standalone",
+  webpack(config, { isServer, dev }) {
+    // Use the client static directory in the server bundle and prod mode
+    // Fixes `Error occurred prerendering page "/"`
+    config.output.webassemblyModuleFilename =
+      isServer && !dev
+        ? "../static/wasm/[modulehash].wasm"
+        : "static/wasm/[modulehash].wasm";
 
-      // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-      config.experiments = { ...config.experiments, asyncWebAssembly: true };
-      // =========================================================================
-      // Grab the existing rule that handles SVG imports
-      const fileLoaderRule = config.module.rules.find((rule) =>
-        rule.test?.test?.(".svg")
-      );
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    // =========================================================================
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
 
-      config.module.rules.push(
-        // Reapply the existing rule, but only for svg imports ending in ?url
-        {
-          ...fileLoaderRule,
-          test: /\.svg$/i,
-          resourceQuery: /url/, // *.svg?url
-        },
-        // Convert all other *.svg imports to React components
-        {
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          resourceQuery: { not: /url/ }, // exclude if *.svg?url
-          use: ["@svgr/webpack"],
-        }
-      );
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
 
-      // Modify the file loader rule to ignore *.svg, since we have it handled now.
-      fileLoaderRule.exclude = /\.svg$/i;
-      return config;
-    },
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
   async redirects() {
     return [
       {
